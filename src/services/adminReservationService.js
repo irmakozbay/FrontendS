665042ce325cf8e3ddcf8591fc1ae9fdf05d3c44
miraleva <miramirza74@gmail.com>
2026-07-28 11:16:15 +0300

    sidebar istek

--- src/services/adminReservationService.js
+++ src/services/adminReservationService.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL || "http://localhost:8083";
+    import.meta.env.VITE_API_URL || import.meta.env.VITE_API_BASE_URL || "http://localhost:8083";
 
 export async function getReservations() {
     const token = localStorage.getItem("adminToken") || sessionStorage.getItem("adminToken");
